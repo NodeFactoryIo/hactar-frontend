61c8eab1dc10f68e0a31f4d62daac547c94275c0
Belma Gutlic <belma.dzi@gmail.com>
2020-01-22 15:22:45 +0100

    Fix eslint

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,9 +9,7 @@ interface IBaseButtonProps extends React.HTMLProps<HTMLButtonElement> {
     type: string;
 }
 
-const BaseButton = ({type, ...props}: IBaseButtonProps) => (
-    <button {...props} className={`button btn-${type}`} />
-);
+const BaseButton = ({type, ...props}: IBaseButtonProps) => <button {...props} className={`button btn-${type}`} />;
 
 export const ButtonPrimary = (props: React.HTMLProps<HTMLButtonElement>) => (
     <BaseButton type={TYPES.PRIMARY} {...props} />
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -30,4 +30,4 @@ export const Input = (props: IInputContainerProps) => {
             <input {...props} />
         </div>
     );
-}
+};
