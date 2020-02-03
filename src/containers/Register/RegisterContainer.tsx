abfec97c4ea119b097aaae9385e5b4176b200b5e
morrigan <belma.dzi@gmail.com>
2020-02-03 14:55:10 +0100

    Fix background image on onboarding strech

--- src/containers/Register/RegisterContainer.tsx
+++ src/containers/Register/RegisterContainer.tsx
@@ -1,12 +1,15 @@
 import React, {ReactElement} from "react";
 
+import BackgroundImage from "../../assets/images/background.svg";
 import {Routes} from "../../constants/routes";
 import {RegisterForm} from "./RegisterForm";
 
 export const RegisterContainer = (): ReactElement => {
     return (
         <div className="onboarding-container">
-            <h1>
+            <img src={BackgroundImage} className="background-image" />
+
+            <h1 className="elevated">
                 An analysis tool for your{" "}
                 <a href="http://filecoin.io" target="_blank" rel="noopener noreferrer">
                     Filecoin
@@ -14,7 +17,7 @@ export const RegisterContainer = (): ReactElement => {
                 mining nodes.
             </h1>
 
-            <div className="flex-column">
+            <div className="flex-column elevated">
                 <div className="form-container flex-column">
                     <div className="logo-horizontal self-centered" />
 
