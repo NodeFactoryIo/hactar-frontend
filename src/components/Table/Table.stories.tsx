59ec8aeca01bdce7cc81dc305dfdd6fd30cdf086
morrigan <belma.dzi@gmail.com>
2020-02-03 09:47:41 +0100

    Display columns generically

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -3,6 +3,20 @@ import { storiesOf } from '@storybook/react';
 import { Table } from './Table';
 import '../../style/index.scss'
 
-storiesOf('Table', module).add('Table', () => {
-    return <Table />;
+storiesOf('Table', module).add('Deals', () => {
+    const data = [
+        {id: 'baf5relasidadasidadmv', status: 'Rejected', provider: "t01440", size: "254 bytes", price: "0.5", duration: 12},
+        {id: 'jf3ij56lasdakdja', status: 'Accepted', provider: "t01440", size: "254 bytes", price: "0.4", duration: 25},
+    ];
+
+    const columns = [
+        { key: "id" },
+        { key: "status" },
+        { key: "provider" },
+        { key: "size" },
+        { key: "price", label: "Total price (FIL)" },
+        { key: "duration", label: "Duration (blocks)" },
+    ];
+
+    return <Table data={data} columns={columns} />;
 });
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import { Table as RTable, Column } from "react-virtualized";
 
-export const Table = () => {
-    const list = [
-        {name: 'Brian Vaughn', description: 'Software engineer', status: "Accepted"},
-        {name: 'Alan Ford', description: 'CEO', status: "Unknown"},
-        {name: 'Dylan Dog', description: 'Fixer', status: "Failed"},
-    ];
+interface ITableProps {
+    data: Object[];
+    columns: any[];
+}
 
+export const Table = ({ data, columns }: ITableProps) => {
     return (
         <RTable
             width={1000}
             height={400}
             headerHeight={20}
             rowHeight={30}
-            rowCount={list.length}
-            rowGetter={({index}) => list[index]}
+            rowCount={data.length}
+            rowGetter={({index}) => data[index]}
         >
-            <Column label="Name" dataKey="name" width={200} />
-            <Column label="Status" dataKey="status" width={200} />
-            <Column width={300} label="Description" dataKey="description" />
+            {columns.map((column, index) => (
+                <Column
+                    key={index}
+                    label={column.label || column.key}
+                    dataKey={column.key}
+                    width={200}
+                />
+            ))}
         </RTable>
     );
 }
