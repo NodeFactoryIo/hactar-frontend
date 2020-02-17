33eb0988836e761acb7a1ee61643ccdd1d587561
irubido <ivan.rubido@gmail.com>
2020-02-17 13:28:25 +0100

    copy to clipboard

---
+++ src/components/Clipboard/Clipboard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {TableCellProps} from "react-virtualized";
+import CopyToClipboard from "react-copy-to-clipboard";
+
+export interface IClipboardProps {
+    copyText: string;
+}
+
+export const ClipboardTable = ({cellData}: TableCellProps): JSX.Element => {
+    return (
+        <>
+            <span>{cellData}</span>
+            <CopyToClipboard text={cellData}>
+                <button className="copy_icon"></button>
+            </CopyToClipboard>
+        </>
+    );
+};
+
+export const Clipboard = ({copyText}: IClipboardProps): JSX.Element => {
+    return (
+        <div className="clipboard_container">
+            <p>{copyText}</p>
+            <CopyToClipboard text={copyText}>
+                <button className="copy_icon"></button>
+            </CopyToClipboard>
+        </div>
+    );
+};
