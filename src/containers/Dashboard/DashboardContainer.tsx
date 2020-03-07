dc07344ece28ade440e28404634218a9d198dca1
morrigan <belma.dzi@gmail.com>
2020-03-07 19:14:58 +0100

    Remove fake email from top bar

--- src/containers/Dashboard/DashboardContainer.tsx
+++ src/containers/Dashboard/DashboardContainer.tsx
@@ -40,7 +40,7 @@ export const DashboardContainer = (): ReactElement => {
 
     return (
         <div className="dashboard-container">
-            <TopBar logOut={() => dispatch(logOutUser())} email="johndoe@nodefactory.io" />
+            <TopBar logOut={() => dispatch(logOutUser())} email="User Account" />
 
             {!stateNodeList.isLoading && !selectedNodeId && stateNodeList.data.length === 0 ? (
                 <EmptyList message="No nodes are added" />
