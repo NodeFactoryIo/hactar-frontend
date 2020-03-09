a4b7ceba5dfa975df387177daf2845d56561cb32
morrigan <belma.dzi@gmail.com>
2020-03-09 16:22:53 +0100

    Fix formatting balance

--- src/containers/Balance/CurrentBalanceContainer.tsx
+++ src/containers/Balance/CurrentBalanceContainer.tsx
@@ -1,7 +1,8 @@
 import React, {ReactElement, useEffect} from "react";
-import "./balance.scss";
+import BigNumber from "bignumber.js"
 import {useDispatch, useSelector} from "react-redux";
 
+import "./balance.scss";
 import {getBalanceInfo} from "./BalanceSlice";
 import {RootState} from "../../app/rootReducer";
 
@@ -15,6 +16,10 @@ export const CurrentBalanceContainer: React.FC = (): ReactElement => {
             dispatch(getBalanceInfo(selectedNodeId));
         }
     }, [selectedNodeId, dispatch]);
+
+    const formatBalance = (balance: string): string => {
+        return new BigNumber(balance).toFormat(2);
+    };
 
     if (balance.isLoading) {
         return (
@@ -33,9 +38,9 @@ export const CurrentBalanceContainer: React.FC = (): ReactElement => {
             </div>
 
             <div className="lower balance">
-                <h2>{balance.data.currentBalance} FIL</h2>
+                <h2>{formatBalance(balance.data.currentBalance)} FIL</h2>
                 <p className="yellow">
-                    + {balance.data.balanceChange} FIL ({balance.data.balanceChangePerc})
+                    {formatBalance(balance.data.balanceChange)} FIL ({balance.data.balanceChangePerc})
                 </p>
             </div>
         </div>
